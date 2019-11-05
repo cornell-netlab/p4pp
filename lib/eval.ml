1981face3b29619d42db5c17a5bf10d21730b93f
Alex Chang <apc73@cornell.edu>
2019-11-05 13:37:02 -0500

    add method to process strings directly

--- lib/eval.ml
+++ lib/eval.ml
@@ -1,34 +1,34 @@
 open Core
 open Ast
 
-type env = 
+type env =
   { file : string;
     defines : (string * Int64.t) list }
 let empty file =
    { file; defines = [] }
-let is_defined env m = 
+let is_defined env m =
   List.Assoc.find ~equal:(=) env.defines m <> None
-let define env m = 
-  if is_defined env m then env 
+let define env m =
+  if is_defined env m then env
   else { env with defines = List.Assoc.add ~equal:(=) env.defines m Int64.zero }
-let undefine env m = 
-  { env with 
+let undefine env m =
+  { env with
     defines = List.Assoc.remove ~equal:(=) env.defines m }
 let get_file env = env.file
 let set_file env file = { env with file }
 
-let rec find includes file = 
-  match includes with 
-  | [] -> 
+let rec find includes file =
+  match includes with
+  | [] ->
      failwith ("Error: " ^ file ^ " could not be found")
-  | h::t -> 
+  | h::t ->
      let path = Filename.concat h file in
      if Sys.file_exists path = `Yes then path
      else find t file
 
-let eval_binop (bop:bop) = 
+let eval_binop (bop:bop) =
   let open Int64 in
-  match bop with 
+  match bop with
   | Add -> ( + )
   | Sub -> ( - )
   | Mult -> ( * )
@@ -47,64 +47,64 @@ let eval_binop (bop:bop) =
   | And -> (fun n1 n2 -> if n1 <> zero && n2 <> zero then one else zero)
   | Or -> (fun n1 n2 -> if n1 <> zero || n2 <> zero then one else zero)
 
-let eval_uop (uop:uop) = 
+let eval_uop (uop:uop) =
   let open Int64 in
-  match uop with 
+  match uop with
   | BNot -> bit_not
   | Not -> (fun n -> if n = zero then one else zero)
 
-let rec eval_test (env:env) (test:test) : Int64.t = 
-  match test with 
+let rec eval_test (env:env) (test:test) : Int64.t =
+  match test with
   | Int(n) -> n
-  | Defined(m) -> 
+  | Defined(m) ->
      if is_defined env m then Int64.zero
      else Int64.one
-  | Ident(m) -> 
+  | Ident(m) ->
      Int64.zero
-  | BinOp(test1,bop,test2) -> 
+  | BinOp(test1,bop,test2) ->
      eval_binop bop (eval_test env test1) (eval_test env test2)
-  | UnOp(uop,test1) -> 
+  | UnOp(uop,test1) ->
      eval_uop uop (eval_test env test1)
 
-let rec eval (includes:string list) (env:env) (buf:Buffer.t) (term:term) : env = 
-  let current = get_file env in 
-  match term with 
-  | String(s) -> 
+let rec eval (includes:string list) (env:env) (buf:Buffer.t) (term:term) : env =
+  let current = get_file env in
+  match term with
+  | String(s) ->
      Buffer.add_string buf (Printf.sprintf "\"%s\"" s);
      env
-  | Text(s) -> 
+  | Text(s) ->
      Buffer.add_string buf s;
      env
-  | Include(line,search,file) -> 
-     let path = find includes file in 
+  | Include(line,search,file) ->
+     let path = find includes file in
      let env = set_file env path in
      let env = preprocess_file includes env buf path in
      let env = set_file env current in
      Buffer.add_string buf "\n";
      Buffer.add_string buf (Printf.sprintf "#line %d \"%s\" %d\n" line current 2);
      env
-  | Define(m) -> 
-     let env = define env m in 
+  | Define(m) ->
+     let env = define env m in
      Buffer.add_string buf "\n";
      env
-  | Undef(m) -> 
-     let env = undefine env m in 
+  | Undef(m) ->
+     let env = undefine env m in
      Buffer.add_string buf "\n";
      env
   | IfDef(macro,line_tru,tru,line_fls,fls,line_end) ->
-     let b = is_defined env macro in 
+     let b = is_defined env macro in
      cond includes env buf b line_tru tru line_fls fls line_end
   | IfNDef(macro,line_tru,tru,line_fls,fls,line_end) ->
-     let b = not(is_defined env macro) in 
+     let b = not(is_defined env macro) in
      cond includes env buf b line_tru tru line_fls fls line_end
-  | If(test,line_tru, tru, line_fls, fls, line_end) -> 
-     let b = Int64.zero = eval_test env test in 
+  | If(test,line_tru, tru, line_fls, fls, line_end) ->
+     let b = Int64.zero = eval_test env test in
      cond includes env buf b line_tru tru line_fls fls line_end
-     
-and cond includes env buf b line_tru tru line_fls fls line_end = 
-  let current = get_file env in 
-  let env = 
-    if b then 
+
+and cond includes env buf b line_tru tru line_fls fls line_end =
+  let current = get_file env in
+  let env =
+    if b then
       begin
         Buffer.add_string buf (Printf.sprintf "#line %d \"%s\"\n" line_tru current);
         List.fold_left ~init:env ~f:(fun env term -> eval includes env buf term) tru
@@ -112,21 +112,27 @@ and cond includes env buf b line_tru tru line_fls fls line_end =
     else
       begin
         Buffer.add_string buf (Printf.sprintf "#line %d \"%s\"\n" line_fls current);              List.fold_left ~init:env ~f:(fun env term -> eval includes env buf term) fls
-      end in 
+      end in
   Buffer.add_string buf (Printf.sprintf "#line %d \"%s\"\n" line_end current);
   env
 
-and preprocess_file (includes:string list) (env:env) (buf:Buffer.t) (file:string) : env = 
-  let () = Buffer.add_string buf (Printf.sprintf "#line %d \"%s\" %d\n" 1 file 1) in 
+and preprocess_string (includes:string list) (env:env) (buf:Buffer.t) (file_contents:string) : env =
+  let () = Buffer.add_string buf (Printf.sprintf "#line %d \"%s\" %d\n" 1 "typed_input.p4" 1) in
+  let lexbuf = Lexing.from_string file_contents |> Prelexer.lex |> Lexing.from_string in
+  let terms =
+    try Parser.program Lexer.token lexbuf
+    with _ -> failwith ("Error parsing " ^ "typed input" ^ " : " ^ string_of_int (!Lexer.current_line)) in
+  List.fold_left ~init:env ~f:(fun env term -> eval includes env buf term) terms
+
+and preprocess_file (includes:string list) (env:env) (buf:Buffer.t) (file:string) : env =
+  let () = Buffer.add_string buf (Printf.sprintf "#line %d \"%s\" %d\n" 1 file 1) in
   let channel = In_channel.create file in
-  let lexbuf = Lexing.from_channel channel in 
-  let () = Prelexer.reset file in 
+  let lexbuf = Lexing.from_channel channel in
+  let () = Prelexer.reset file in
   let string = Prelexer.lex lexbuf in
   let () = In_channel.close channel in
   let lexbuf = Lexing.from_string string in
-  let terms = 
-    try Parser.program Lexer.token lexbuf 
-    with _ -> failwith ("Error parsing " ^ file ^ " : " ^ string_of_int (!Lexer.current_line)) in 
+  let terms =
+    try Parser.program Lexer.token lexbuf
+    with _ -> failwith ("Error parsing " ^ file ^ " : " ^ string_of_int (!Lexer.current_line)) in
   List.fold_left ~init:env ~f:(fun env term -> eval includes env buf term) terms
-
-
